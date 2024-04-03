ad79d325c81705054bb7a0b24ed710847cd0a426
Chrispine Oburu <chrizoburu65@gmail.com>
2024-04-03 22:30:34 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  //To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(function(books) {
+      return renderBooks(books)
+    });
+
+    
 }
 
 function renderBooks(books) {
